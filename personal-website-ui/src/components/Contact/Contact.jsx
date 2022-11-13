0d11800b8500da99c2cb2109203ab0361d05fcbc
katherinj <katjimenez98@gmail.com>
2022-11-13 02:10:30 -0500

    Added skeleton components Contact, About, Portfolio

---
+++ personal-website-ui/src/components/Contact/Contact.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "./Contact.css";
+
+export default function Contact() {
+  return (
+    <div className="contact">
+      <h4> Testing Contact</h4>
+    </div>
+  );
+}
