9cfc8e7fc8c5a36a32519f14040585281487cae4
Katherin Jimenez <katjimenez98@gmail.com>
2024-11-18 18:42:48 -0500

    minor css fixes

--- personal-website-ui/src/components/MyNavbar/MyNavbar.jsx
+++ personal-website-ui/src/components/MyNavbar/MyNavbar.jsx
@@ -2,15 +2,19 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { LinkContainer } from "react-router-bootstrap";
+import "./MyNavbar.css";
 
 export default function MyNavbar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary  mt-3">
       <Container>
         <LinkContainer to="/">
-          <Navbar.Brand className="fw-semibold">Katherin.io</Navbar.Brand>
+          <Navbar.Brand className="fw-semibold"> katherin.io</Navbar.Brand>
         </LinkContainer>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          className="nav-custom-toggle"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <LinkContainer to="/about">
--- personal-website-ui/src/components/index.jsx
+++ personal-website-ui/src/components/index.jsx
@@ -3,5 +3,5 @@ export { default as About } from "./About/About";
 export { default as Contact } from "./Contact/Contact";
 export { default as Footer } from "./Footer/Footer";
 export { default as Landing } from "./Landing/Landing";
-export { default as MyNavbar } from "./Navbar/MyNavbar";
+export { default as MyNavbar } from "./MyNavbar/MyNavbar";
 export { default as Portfolio } from "./Portfolio/Portfolio";
