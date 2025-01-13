1fa99c795c5ed7020bc77b40add4fbc695646bc7
Katherin Jimenez <katjimenez98@gmail.com>
2025-01-13 16:23:50 -0500

    adding picture and link to about me page to button on landing page

--- personal-website-ui/src/components/Landing/Landing.jsx
+++ personal-website-ui/src/components/Landing/Landing.jsx
@@ -1,17 +1,22 @@
 import * as React from "react";
 import { Container, Button } from "react-bootstrap";
-
+import "./Landing.css";
+import { Link } from "react-router-dom";
 export default function Landing() {
   return (
     <section className="bg-light py-5 text-center">
       <Container>
+        <img
+          src="../media/landing-page-profile-pic.jpeg"
+          className="profile-img"
+        />
         <h1 className="display-4 fw-bold">Welcome to Katherin.io</h1>
-        <p className="lead">
-          Build stunning web experiences with simplicity and speed.
-        </p>
-        <Button variant="primary" size="lg" className="mt-3">
-          Learn More
-        </Button>
+        <p className="lead">Software Engineer</p>
+        <Link to="/about">
+          <Button variant="primary" size="lg" className="mt-3">
+            Learn More
+          </Button>
+        </Link>
       </Container>
     </section>
   );
